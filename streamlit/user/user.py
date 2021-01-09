121bdcbe47f695b756afece4c0926b477e2be8d1
Ed <edwardsapp@gmail.com>
2021-01-09 20:26:33 +0000

    adding check user exists to user model

--- streamlit/user/user.py
+++ streamlit/user/user.py
@@ -2,7 +2,10 @@ from sessionstate import _get_full_session
 
 class User():
     def __init__(self):
-        current_username = _get_active_user()
+        self.current_username = _get_active_user()
+
+    def check_user_exists(self):
+        return True
 
 def _get_active_user():
     session = _get_full_session()
