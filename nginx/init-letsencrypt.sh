faf327bf3fea0ee105b2b51ac535d8d9857fed34
Ed <edwardsapp@gmail.com>
2021-01-10 17:47:46 +0000

    updated cert attempt, didnt work tho

--- nginx/init-letsencrypt.sh
+++ nginx/init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=flip.rip
+domains='flip.rip'
 rsa_key_size=4096
 data_path="./nginx/data/certbot"
 email="" # Adding a valid address is strongly recommended
